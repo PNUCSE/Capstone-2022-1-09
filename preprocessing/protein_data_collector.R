f994ad94fdd4a1e60dc33b439439d00d192c19e8
rubpcase <ori844acul@naver.com>
2022-09-24 11:24:00 +0900

    Remove path name
    Remove path name
    - Remove path name

--- preprocessing/protein_data_collector.R
+++ preprocessing/protein_data_collector.R
@@ -11,11 +11,7 @@ BiocManager::install("rcdklibs", force=TRUE)
 # 라이브러리 불러오기
 library("Rcpi")
 
-# 순서: Alx1( Mus musculus ), ARX ( Homo sapiens ), ALX3 ( Homo sapiens ), AR_FL ( Homo sapiens )
-# entry = c('Q8C8B0', 'Q96QS3', 'O95076', 'P10275')
-# label = c('Alx1', 'ARX', 'ALX3', 'AR_FL')
-
-file = "C:/Users/computer/ABCproject/data/protein_label_data.csv"
+file = "경로명/data/protein_label_data.csv"
 labelList = read.csv(file, header=TRUE)
 
 rslt_df = c()
@@ -36,6 +32,6 @@ for(i in 1:nrow(labelList)){
 	rslt_df = rbind(rslt_df, trslt)
 }
 
-directory_path = 'C:/Users/computer/ABCproject/data'
+directory_path = '경로명/data'
 setwd(directory_path)
 write.csv(rslt_df, file='protein_data.csv', quote=FALSE)
